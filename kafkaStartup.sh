14cf94b1a3beab3eafeaad1bd4e81d1cda869c08
puneetmahajan <puneet@persistence.one>
2021-04-30 09:32:16 +0530

    updating kafka version

--- kafkaStartup.sh
+++ kafkaStartup.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-if [ -d "kafka_2.12-2.5.0" ]; then
+
+KAFKA_VERSION=2.8.0
+KAFKA_FOLDER=kafka_2.13-"$KAFKA_VERSION"
+
+if [ -d $KAFKA_FOLDER ]; then
   echo "File exists"
 else
-  echo "File does not exist, downloading"
-  wget http://mirrors.estointernet.in/apache/kafka/2.5.0/kafka_2.12-2.5.0.tgz
-  tar -xzf kafka_2.12-2.5.0.tgz
-  rm kafka_2.12-2.5.0.tgz
+  echo "File does not exist, downloading $KAFKA_VERSION"
+  wget http://mirrors.estointernet.in/apache/kafka/"$KAFKA_VERSION"/"$KAFKA_FOLDER".tgz
+  tar -xzf "$KAFKA_FOLDER".tgz
+  rm "$KAFKA_FOLDER".tgz
 fi
 
-cd kafka_2.12-2.5.0
+cd "$KAFKA_FOLDER"
 
 bin/zookeeper-server-start.sh config/zookeeper.properties &
 
