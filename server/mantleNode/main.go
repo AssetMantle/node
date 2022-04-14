08f322d8a7430bf3b9fc72d32dcffc15088836b8
deepanshutr <deepanshutr@gmail.com>
2022-04-15 00:36:38 +0530

    Updating application configuration.

--- server/mantleNode/main.go
+++ server/mantleNode/main.go
@@ -5,12 +5,19 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/server"
 	serverCmd "github.com/cosmos/cosmos-sdk/server/cmd"
+	sdkTypes "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/AssetMantle/node/application"
 	"github.com/AssetMantle/node/server/mantleNode/commands"
 )
 
 func main() {
+	config := sdkTypes.GetConfig()
+	config.SetBech32PrefixForAccount("mantle", "mantlepub")
+	config.SetBech32PrefixForValidator("mantlevaloper", "mantlevaloper")
+	config.SetBech32PrefixForConsensusNode("mantlevalcons", "mantlevalconspub")
+	config.Seal()
+
 	rootCmd, _ := commands.NewRootCommand()
 
 	if err := serverCmd.Execute(rootCmd, application.DefaultNodeHome); err != nil {
