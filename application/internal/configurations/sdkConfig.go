e2cea2fa172e457b120e19c507b9fee3d751817a
deepanshutr <deepanshutr@gmail.com>
2023-01-02 21:59:40 +0530

    refactor(sdkConfiguration): moving to configuration package and updating impl

---
+++ application/internal/configurations/sdkConfig.go
@@ -0,0 +1,18 @@
+// Copyright [2021] - [2022], AssetMantle Pte. Ltd. and the code contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package configurations
+
+import (
+	sdkTypes "github.com/cosmos/cosmos-sdk/types"
+)
+
+func SetAndSealSDKConfig() {
+	sdkConfig := sdkTypes.GetConfig()
+	sdkConfig.SetBech32PrefixForAccount(bech32Prefix, bech32Prefix+sdkTypes.PrefixPublic)
+	sdkConfig.SetBech32PrefixForValidator(bech32Prefix+sdkTypes.PrefixValidator+sdkTypes.PrefixOperator, bech32Prefix+sdkTypes.PrefixValidator+sdkTypes.PrefixOperator+sdkTypes.PrefixPublic)
+	sdkConfig.SetBech32PrefixForConsensusNode(bech32Prefix+sdkTypes.PrefixValidator+sdkTypes.PrefixConsensus, bech32Prefix+sdkTypes.PrefixValidator+sdkTypes.PrefixConsensus+sdkTypes.PrefixPublic)
+	sdkConfig.SetCoinType(coinType)
+	sdkConfig.SetPurpose(purpose)
+	sdkConfig.Seal()
+}
