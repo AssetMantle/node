ef9bd2698bdbf64e20f47c30a0c5d4ca4eddada3
deepanshutr <deepanshutr@gmail.com>
2023-01-02 22:00:10 +0530

    feat(moduleBasicManager): updating sdk modules

--- application/internal/configurations/modules.go
+++ application/internal/configurations/modules.go
@@ -15,11 +15,15 @@ import (
 	wasmClient "github.com/CosmWasm/wasmd/x/wasm/client"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	"github.com/cosmos/cosmos-sdk/x/auth"
+	"github.com/cosmos/cosmos-sdk/x/auth/vesting"
+	authzModule "github.com/cosmos/cosmos-sdk/x/authz/module"
 	"github.com/cosmos/cosmos-sdk/x/bank"
+	"github.com/cosmos/cosmos-sdk/x/capability"
 	"github.com/cosmos/cosmos-sdk/x/crisis"
 	"github.com/cosmos/cosmos-sdk/x/distribution"
 	distributionClient "github.com/cosmos/cosmos-sdk/x/distribution/client"
 	"github.com/cosmos/cosmos-sdk/x/evidence"
+	feegrantModule "github.com/cosmos/cosmos-sdk/x/feegrant/module"
 	"github.com/cosmos/cosmos-sdk/x/genutil"
 	"github.com/cosmos/cosmos-sdk/x/gov"
 	"github.com/cosmos/cosmos-sdk/x/mint"
@@ -29,23 +33,43 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/staking"
 	"github.com/cosmos/cosmos-sdk/x/upgrade"
 	upgradeClient "github.com/cosmos/cosmos-sdk/x/upgrade/client"
+	ica "github.com/cosmos/ibc-go/v3/modules/apps/27-interchain-accounts"
+	"github.com/cosmos/ibc-go/v3/modules/apps/transfer"
+	ibc "github.com/cosmos/ibc-go/v3/modules/core"
 	ibcClientClient "github.com/cosmos/ibc-go/v3/modules/core/02-client/client"
 )
 
 var ModuleBasicManager = module.NewBasicManager(
-	genutil.AppModuleBasic{},
 	auth.AppModuleBasic{},
+	genutil.AppModuleBasic{},
 	bank.AppModuleBasic{},
+	capability.AppModuleBasic{},
 	staking.AppModuleBasic{},
 	mint.AppModuleBasic{},
 	distribution.AppModuleBasic{},
-	gov.NewAppModuleBasic(append(wasmClient.ProposalHandlers, paramsClient.ProposalHandler, distributionClient.ProposalHandler, upgradeClient.ProposalHandler, upgradeClient.CancelProposalHandler, ibcClientClient.UpdateClientProposalHandler, ibcClientClient.UpgradeProposalHandler)...),
+	gov.NewAppModuleBasic(
+		append(wasmClient.ProposalHandlers,
+			paramsClient.ProposalHandler,
+			distributionClient.ProposalHandler,
+			upgradeClient.ProposalHandler,
+			upgradeClient.CancelProposalHandler,
+			ibcClientClient.UpdateClientProposalHandler,
+			ibcClientClient.UpgradeProposalHandler,
+		)...,
+	),
 	params.AppModuleBasic{},
 	crisis.AppModuleBasic{},
-	wasm.AppModuleBasic{},
 	slashing.AppModuleBasic{},
+	feegrantModule.AppModuleBasic{},
+	authzModule.AppModuleBasic{},
+	ibc.AppModuleBasic{},
 	upgrade.AppModuleBasic{},
 	evidence.AppModuleBasic{},
+	transfer.AppModuleBasic{},
+	vesting.AppModuleBasic{},
+	ica.AppModuleBasic{},
+
+	wasm.AppModuleBasic{},
 
 	assets.Prototype(),
 	classifications.Prototype(),
