f3d113d211be83d3728c1e331e3860e580ebd86b
deepanshutr <deepanshutr@gmail.com>
2024-03-07 15:52:54 +0530

    fix(response_test): The test methods for key addition and signing have been updated. The key addition test is updated to use multisig and secp256k1 keys, providing a more complex and realistic test scenario. The signing method test has been updated to use legacytx instead of authTypes for generating fees and standard transactions, aligning with the updated cosmos-sdk.

--- utilities/rest/keys/add/response_test.go
+++ utilities/rest/keys/add/response_test.go
@@ -4,6 +4,10 @@
 package add
 
 import (
+	"github.com/cosmos/cosmos-sdk/crypto/keys/multisig"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
+	"github.com/cosmos/cosmos-sdk/crypto/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"testing"
 
 	errorConstants "github.com/AssetMantle/schema/go/errors/constants"
@@ -12,7 +16,15 @@ import (
 )
 
 func Test_Add_Response(t *testing.T) {
-	testKeyOutput, _ := keyring.NewKeyOutput("name", "keyType", "address", "pubkey")
+	sk := secp256k1.PrivKey{Key: []byte{154, 49, 3, 117, 55, 232, 249, 20, 205, 216, 102, 7, 136, 72, 177, 2, 131, 202, 234, 81, 31, 208, 46, 244, 179, 192, 167, 163, 142, 117, 246, 13}}
+	tmpKey := sk.PubKey()
+	multisigPk := multisig.NewLegacyAminoPubKey(1, []types.PubKey{tmpKey})
+
+	info, err := keyring.NewMultiInfo("multisig", multisigPk)
+	require.NoError(t, err)
+	accAddr := sdk.AccAddress(info.GetPubKey().Address())
+	testKeyOutput, err := keyring.NewKeyOutput(info.GetName(), info.GetType(), accAddr, multisigPk)
+	require.NoError(t, err)
 	testResponse := newResponse(testKeyOutput, nil)
 	require.Equal(t, response{Success: true, Error: nil, KeyOutput: testKeyOutput}, testResponse)
 	require.Equal(t, true, testResponse.IsSuccessful())
