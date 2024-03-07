51d5476c1eb848eb9afa714099b49b72a5d0ddb7
deepanshutr <deepanshutr@gmail.com>
2024-03-07 15:53:38 +0530

    fix(request_test):The commit updates the 'authTypes' to 'legacytx' in the 'request_test.go' file. This change impacts the 'TestFee' and 'TestStdTx' functions where authTypes has been replaced with legacytx. The commit ensures that the application uses the correct library for these functions.

--- utilities/rest/sign/request_test.go
+++ utilities/rest/sign/request_test.go
@@ -4,11 +4,11 @@
 package sign
 
 import (
+	"github.com/cosmos/cosmos-sdk/x/auth/legacy/legacytx"
 	"testing"
 
 	sdkTypes "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/rest"
-	authTypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	"github.com/stretchr/testify/require"
 )
 
@@ -16,8 +16,8 @@ func Test_SignTx_Request(t *testing.T) {
 	fromAddress := "cosmos1pkkayn066msg6kn33wnl5srhdt3tnu2vzasz9c"
 	testBaseReq := rest.BaseReq{From: fromAddress, ChainID: "test", Fees: sdkTypes.NewCoins()}
 
-	testFee := authTypes.NewStdFee(12, sdkTypes.NewCoins())
+	testFee := legacytx.NewStdFee(12, sdkTypes.NewCoins())
 
-	testStdTx := authTypes.NewStdTx([]sdkTypes.Msg{}, testFee, []authTypes.StdSignature{}, "")
+	testStdTx := legacytx.NewStdTx([]sdkTypes.Msg{}, testFee, []legacytx.StdSignature{}, "")
 	require.Equal(t, nil, request{BaseRequest: testBaseReq, Type: "type", StdTx: testStdTx}.Validate())
 }
