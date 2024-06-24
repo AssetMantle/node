7e1b8bb598aef1395889c4a5c9f6140e2fb74419
Deepanshu Tripathi <deepanshutr@gmail.com>
2024-06-24 16:07:27 +0530

    refactor: sdk upgrade refactoring
    Signed-off-by: Deepanshu Tripathi <deepanshutr@gmail.com>

--- application/types/applications/application.go
+++ application/types/applications/application.go
@@ -4,16 +4,16 @@
 package applications
 
 import (
+	storeTypes "github.com/cosmos/cosmos-sdk/store/types"
 	"io"
 
+	tendermintDB "github.com/cometbft/cometbft-db"
+	"github.com/cometbft/cometbft/libs/log"
+	tendermintLog "github.com/cometbft/cometbft/libs/log"
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	serverTypes "github.com/cosmos/cosmos-sdk/server/types"
-	sdkTypes "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	"github.com/spf13/cobra"
-	"github.com/tendermint/tendermint/libs/log"
-	tendermintLog "github.com/tendermint/tendermint/libs/log"
-	tendermintDB "github.com/tendermint/tm-db"
 
 	"github.com/AssetMantle/modules/helpers"
 )
@@ -27,23 +27,23 @@ type Application interface {
 	GetCodec() helpers.Codec
 
 	LoadHeight(int64) error
-	ExportApplicationStateAndValidators(bool, []string) (serverTypes.ExportedApp, error)
+	ExportApplicationStateAndValidators(bool, []string, []string) (serverTypes.ExportedApp, error)
 
 	Name() string
 	Logger() log.Logger
-	MountStores(keys ...sdkTypes.StoreKey)
-	MountKVStores(keys map[string]*sdkTypes.KVStoreKey)
-	MountTransientStores(keys map[string]*sdkTypes.TransientStoreKey)
-	MountStore(key sdkTypes.StoreKey, typ sdkTypes.StoreType)
-	LastCommitID() sdkTypes.CommitID
+	MountStores(keys ...storeTypes.StoreKey)
+	MountKVStores(keys map[string]*storeTypes.KVStoreKey)
+	MountTransientStores(keys map[string]*storeTypes.TransientStoreKey)
+	MountStore(key storeTypes.StoreKey, typ storeTypes.StoreType)
+	LastCommitID() storeTypes.CommitID
 	LastBlockHeight() int64
-	Router() sdkTypes.Router
-	QueryRouter() sdkTypes.QueryRouter
+	//Router() storeTypes.Router
+	//QueryRouter() sdkTypes.QueryRouter
 	Seal()
 	IsSealed() bool
 
 	AppCreator(log.Logger, tendermintDB.DB, io.Writer, serverTypes.AppOptions) serverTypes.Application
-	AppExporter(log.Logger, tendermintDB.DB, io.Writer, int64, bool, []string, serverTypes.AppOptions) (serverTypes.ExportedApp, error)
+	AppExporter(log.Logger, tendermintDB.DB, io.Writer, int64, bool, []string, serverTypes.AppOptions, []string) (serverTypes.ExportedApp, error)
 	ModuleInitFlags(startCmd *cobra.Command)
 
 	Initialize(logger tendermintLog.Logger, db tendermintDB.DB, traceStore io.Writer, loadLatest bool, invCheckPeriod uint, skipUpgradeHeights map[int64]bool, home string, appOptions serverTypes.AppOptions, baseAppOptions ...func(*baseapp.BaseApp)) Application
