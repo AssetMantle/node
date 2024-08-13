5eaf0b8bca2da051eaff3a9b8c9fb78581bfae71
Deepanshu Tripathi <deepanshutr@gmail.com>
2024-08-13 15:35:34 +0530

    minor code optimization
    Signed-off-by: Deepanshu Tripathi <deepanshutr@gmail.com>

--- utilities/rest/initializer.go
+++ utilities/rest/initializer.go
@@ -1,17 +1,17 @@
 package rest
 
 import (
-	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/gorilla/mux"
-
+	documentIDGetters "github.com/AssetMantle/modules/utilities/rest/id_getters/docs"
+	"github.com/AssetMantle/node/utilities/rest/faucet"
 	"github.com/AssetMantle/node/utilities/rest/keys/add"
 	"github.com/AssetMantle/node/utilities/rest/sign"
-
-	"github.com/AssetMantle/node/utilities/rest/faucet"
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/gorilla/mux"
 )
 
 func RegisterRESTRoutes(context client.Context, router *mux.Router) {
 	add.RegisterRESTRoutes(context, router)
 	faucet.RegisterRESTRoutes(context, router)
 	sign.RegisterRESTRoutes(context, router)
+	documentIDGetters.RegisterRESTRoutes(context, router)
 }
