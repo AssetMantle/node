fb444a8eccbb05b1cdcd17e0cfc1bbb459513565
deepanshutr <deepanshutr@gmail.com>
2024-12-12 18:42:42 +0530

    feat: updating voting period parameter to 10s setup

--- .script/setup.sh
+++ .script/setup.sh
@@ -15,3 +15,5 @@ $AM_SEED_1"
 mantleNode genesis add-genesis-account "$AM_NAME_1" "$AM_GENESIS_BALANCE_1$AM_STAKE_DENOMINATION" --keyring-backend "$AM_KEYRING_BACKEND"
 mantleNode genesis gentx "$AM_NAME_1" "$AM_GENESIS_STAKE_1$AM_STAKE_DENOMINATION" --chain-id "$AM_CHAIN_ID" --keyring-backend "$AM_KEYRING_BACKEND"
 mantleNode genesis collect-gentxs
+# Update the voting period parameter in genesis.json to 10s
+jq '.app_state.gov.params.voting_period = "10s"' $HOME/.mantleNode/config/genesis.json > $HOME/.mantleNode/config/genesis_tmp.json && mv $HOME/.mantleNode/config/genesis_tmp.json $HOME/.mantleNode/config/genesis.json
